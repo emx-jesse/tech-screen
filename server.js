7fdd1db6fe48ba898308a08acbc76ed441647177
emx-jesse <jesse.chua@emxdigital.com>
2019-03-04 18:05:00 -0800

    Initial commit.

---
+++ server.js
@@ -0,0 +1,41 @@
+// require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const PORT = process.env.PORT;
+
+app.use(morgan('short'));
+
+app.use('/', (req, res) => {
+  const query = req.query.q;
+  let response = 'OK';
+
+  if (query === 'Degree') {
+    response = process.env.DEGREE;
+  } else if (query === 'Email Address') {
+    response = process.env.EMAIL;
+  } else if (query === 'Status') {
+    response = process.env.STATUS;
+  } else if (query === 'Resume') {
+    response = process.env.RESUME;
+  } else if (query === 'Referrer') {
+    response = process.env.REFERRER;
+  } else if (query === 'Phone') {
+    response = process.env.PHONE;
+  } else if (query === 'Name') {
+    response = process.env.NAME;
+  } else if (query === 'Source') {
+    response = process.env.SOURCE;
+  } else if (query === 'Position') {
+    response = process.env.POSITION;
+  } else if (query === 'Years') {
+    response = process.env.YEARS;
+  } else if (query === 'Puzzle') {
+    const puzzle = req.query.d;
+    const rows = puzzle.split('\n');
+  }
+
+  res.send(response);
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
